c3c6c19a18659a08dd31608c2da422361c13df49
Ryan Whittingham <whittingham777@gmail.com>
2020-12-08 12:15:33 -0800

    Remove header navigation button

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -15,9 +15,6 @@ const Header = ({onHideNav, onShowNav, showNav, siteTitle}) => (
         <div className={styles.twitter}><a href="https://twitter.com/ry_whittingham" target="_blank">Twitter</a></div>
         <div className={styles.github}><a href="https://github.com/ryan-whittingham-v1" target="_blank">GitHub</a></div>
       </div>
-      <button className={styles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}>
-        <Icon symbol='hamburger' />
-      </button>
       {/*
       <nav className={cn(styles.nav, showNav && styles.showNav)}>
         <ul>
